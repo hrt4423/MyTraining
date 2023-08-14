3cabdc2012125336453ed8b20783fa3b97ca1fa1
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2023-08-15 04:42:15 +0900

    CountDownTimerとアニメーションの連動

--- app/src/main/java/rata4423/orange/mytraining/MainActivity.kt
+++ app/src/main/java/rata4423/orange/mytraining/MainActivity.kt
@@ -1,6 +1,7 @@
 package rata4423.orange.mytraining
 
 import android.os.Bundle
+import android.os.CountDownTimer
 //import android.util.Size
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,13 +15,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import rata4423.orange.mytraining.ui.theme.MyTrainingTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +40,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
@@ -48,24 +56,87 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Meter() {
+fun Meter(_progress: Float = 0f) {
+    val circleAngle = 360f //円の角度（固定）
+    val max = 300f //進捗の最大値
+    val angle = 240f //メーターの角度
+    val progressWidth = 14.dp //メーターの幅
+    val backgroundWidth = 18.dp //外枠を描画する図形の幅
+    /*
+        描画開始位置の計算
+        メーターの角度を変更したくなった場合に、angleさえ変更すれば開始位置を計算してくれる
+     */
+    val startAngle = (circleAngle / 4) + ((circleAngle - angle) / 2)
     Canvas(
         modifier = Modifier
-            .size(200.dp)
-            .padding(8.dp),
+            .size(150.dp)
+            .padding(10.dp),
         onDraw = {
+            //メーター:外枠
             drawArc(
                 color = Color.Black,
                 //描画開始位置を指定（0：右, 90：下, 180：左, 270：上）
-                startAngle = 270f,
+                startAngle = startAngle,
                 //実際に描画する角度
-                sweepAngle = 180f,
+                sweepAngle = angle,
                 useCenter = false,
                 size = Size(size.width, size.height),
-                style = Stroke(width = 20f)
+                style = Stroke(
+                    width = backgroundWidth.toPx(),
+                    cap = StrokeCap.Round
+                )
+            )
+            //メーター:背景
+            drawArc(
+                color = Color.DarkGray,
+                startAngle = startAngle,
+                sweepAngle = angle,
+                useCenter = false,
+                size = Size(size.width, size.height),
+                style = Stroke(width = progressWidth.toPx(), cap = StrokeCap.Round)
+            )
+            //進捗の描画
+            drawArc(
+                color = Color.Cyan,
+                startAngle = startAngle,
+                sweepAngle = angle / max * _progress,
+                useCenter = false,
+                size = Size(size.width, size.height),
+                style = Stroke(
+                    width = progressWidth.toPx(),
+                    cap = StrokeCap.Round
+                )
             )
         }
     )
+}
+
+class HomeViewModel: ViewModel() {
+    val progressValue = MutableLiveData(0f)
+    fun valueUp() {
+        val tmp = progressValue.value ?: 0f
+        progressValue.value = tmp + 1f
+    }
+}
+
+@Composable
+fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
+    val progressValue: Float by viewModel.progressValue.observeAsState(0f)
+
+    val timer = object : CountDownTimer(3000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                viewModel.valueUp()
+            }
+            override fun onFinish() {
+                
+            }
+        }
+    Column(){
+        Greeting("Android")
+
+        Meter(progressValue)
+        timer.start()
+    }
 }
 
 @Preview(showBackground = true)
@@ -74,7 +145,7 @@ fun GreetingPreview() {
     MyTrainingTheme {
         Column{
             Greeting("Android")
-            Meter()
+            Meter(200f)
         }
     }
 }
