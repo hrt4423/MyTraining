7805894a89bbee5a9a78b51778ce4137970775aa
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2023-08-14 23:19:58 +0900

    wip

--- app/src/main/java/rata4423/orange/mytraining/MainActivity.kt
+++ app/src/main/java/rata4423/orange/mytraining/MainActivity.kt
@@ -1,15 +1,25 @@
 package rata4423.orange.mytraining
 
 import android.os.Bundle
+//import android.util.Size
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+//import androidx.annotation.Size
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import rata4423.orange.mytraining.ui.theme.MyTrainingTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,10 +47,34 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun Meter() {
+    Canvas(
+        modifier = Modifier
+            .size(200.dp)
+            .padding(8.dp),
+        onDraw = {
+            drawArc(
+                color = Color.Black,
+                //描画開始位置を指定（0：右, 90：下, 180：左, 270：上）
+                startAngle = 270f,
+                //実際に描画する角度
+                sweepAngle = 180f,
+                useCenter = false,
+                size = Size(size.width, size.height),
+                style = Stroke(width = 20f)
+            )
+        }
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyTrainingTheme {
-        Greeting("Android")
+        Column{
+            Greeting("Android")
+            Meter()
+        }
     }
 }
